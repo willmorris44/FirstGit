07fb735ca3302e0ef19d29a5e5bdef033fbdab0a
Will morris <willmorris@Wills-MacBook-Pro.local>
2019-05-03 15:38:46 -0600

    fixed veiwController

--- GitLesson/GitLesson/ViewController.swift
+++ GitLesson/GitLesson/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    func bigChange() {
+        print("the goods")
+    }
 
 
 }
